46f453c5616366b91922142f8f85185694d2a3ac
Edward Tanguay (windows) <edward@tanguay.info>
2025-05-19 16:17:09 +0200

    publishing to pypi

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+from commands import *
 from et772_python_pip import *
+
+help()
 
 message = capitalize_first_letter("hello")
 print(message)
--- setup.py
+++ setup.py
@@ -2,13 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name='et772_python_pip',
-    version='0.1',
+    version='0.3.0',
     packages=find_packages(),
     install_requires=[
     ],
     entry_points={
         "console_scripts": [
-            "et772_python_pip = et772_python_pip.main:main",
+            "piphelp = commands:help",
         ]
-    }
+    },
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown'
 )
